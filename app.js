11034407fc193f910435a920f838b1580a84fdf6
inertiabeer <986619667@qq.com>
2017-03-03 14:21:01 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -11,9 +11,6 @@ var dbURL = 'mongodb://localhost:27017/ife';
 var db = mongoose.connect(dbURL);
 var model = require('./model.js');
 var crypto = require('crypto');
-var async = require('async');
-var child_process = require('child_process');
-
 
 function md5(text) {
 	return crypto.createHash('md5').update(text).digest('hex');
@@ -23,9 +20,6 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-
 app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/public/images'))
 	// view engine setup
@@ -49,120 +43,57 @@ app.post('/baidu', function(req, res) {
 
 	var child_process = require('child_process');
 	var exec = child_process.exec;
-
-	exec("phantomjs task2.js " + req.body.keyword + " " + req.body.phone + " " + req.body.pages,
-		function(err, stdout, stderr) {
-			if (err) {
-				console.log('child_process had some error' + err.code);
-				return;
-			}
-			console.log(stdout);
-			var obj = JSON.parse(stdout);
-			var imgUrls = new Array();
-			for (var i = 0; i < obj.dataList.length; i++) {
-				if (obj.dataList[i].img !== "") {
-					let url = obj.dataList[i].img;
-					imgUrls.push(url);
-				}
-
-			}
-			console.log(imgUrls);
-
-			for (let j = 0; j < imgUrls.length; j++) {
-				request(imgUrls[j]).pipe(fs.createWriteStream("public/images/" + md5(imgUrls[j]) + ".png"));
+	exec("phantomjs task2.js " + req.body.keyword + " " + req.body.phone, function(err, stdout, stderr) {
+		if (err) {
+			console.log('child_process had some error' + err.code);
+			return;
+		}
+		console.log(stdout);
+		var obj = JSON.parse(stdout);
+		var imgUrls = new Array();
+		for (var i = 0; i < obj.dataList.length; i++) {
+			if (obj.dataList[i].img !== "") {
+				let url = obj.dataList[i].img;
+				imgUrls.push(url);
 			}
 
+		}
+		console.log(imgUrls);
+
+		for (let j = 0; j < imgUrls.length; j++) {
+			request(imgUrls[j]).pipe(fs.createWriteStream("public/images/" + md5(imgUrls[j]) + ".png"));
+		}
 
 
-			var newdata = new model({
 
-				device: obj.device,
-				code: obj.code,
-				msg: obj.msg,
-				word: obj.word,
-				time: obj.time,
-				dataList: obj.dataList
+		var newdata = new model({
 
-			});
-			newdata.save(function(err, data) {
-				if (err)
-					console.log(err);
-				else {
-					console.log(data);
+			device: obj.device,
+			code: obj.code,
+			msg: obj.msg,
+			word: obj.word,
+			time: obj.time,
+			dataList: obj.dataList
 
+		});
+		newdata.save(function(err, data) {
+			if (err)
+				console.log(err);
+			else {
+				console.log(data);
+				res.send(obj.dataList);
 
-				}
-
-			});
-
-
+			}
 
 		});
 
 
 
+	});
+
+
 })
-var exec = child_process.exec;
-io.on('connection', function(socket) {
-	socket.on('client', function(content) {
-		async.mapLimit(content, 5, function(item, callback) {
-				exec("phantomjs task2.js " + item.keyword + " " + item.device + " " + item.page,
-					function(err, stdout, stderr) {
-						socket.emit('server', stdout);
-						var obj = JSON.parse(stdout);
-						var imgUrls = new Array();
-						for (var i = 0; i < obj.dataList.length; i++) {
-							if (obj.dataList[i].img !== "") {
-								let url = obj.dataList[i].img;
-								imgUrls.push(url);
-							}
 
-						}
-						console.log(imgUrls);
-
-						for (let j = 0; j < imgUrls.length; j++) {
-							request(imgUrls[j]).pipe(fs.createWriteStream("public/images/" + md5(imgUrls[j]) + ".png"));
-						}
-
-
-
-						var newdata = new model({
-
-							device: obj.device,
-							code: obj.code,
-							msg: obj.msg,
-							word: obj.word,
-							time: obj.time,
-							dataList: obj.dataList
-
-						});
-						newdata.save(function(err, data) {
-							if (err)
-								console.log(err);
-							else {
-								console.log(data);
-
-
-							}
-
-						});
-
-
-					})
-				callback(false);
-
-
-			},
-			function(err, result) {
-				if (err)
-					console.log(err);
-				else
-					console.log('爬取完成');
-			})
-
-
-	})
-})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -182,4 +113,4 @@ app.use(function(err, req, res, next) {
 	res.render('error');
 });
 
-server.listen(3000);
+app.listen(3000);
